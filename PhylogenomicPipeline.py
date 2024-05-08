936876f638035e867af9c1556394e767f83c5442
Mills <millsbro@submit1.localdomain>
2024-05-08 16:29:48 -0700

    added pseudocode

--- PhylogenomicPipeline.py
+++ PhylogenomicPipeline.py
@@ -1,2 +1,13 @@
 ### PHYLOGENOMIC PIPELINE PYTHON SCRIPT ###
 
+## pseudo code starts here ##
+
+# 1. perform multiple sequence alignmnet with mafft
+
+# mafft [input-seq-file] > [name-of-new-file]
+
+# 2. Phylogenetic Inference with IQtree
+
+# new code from thursday
+
+# 3. Tree Topology Analysis
